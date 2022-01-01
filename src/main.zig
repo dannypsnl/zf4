adf9b8f173c6aafa4a3b32cf95bfb4bf6eb843e1
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-01 20:41:44 +0800

    extract vm type!

--- src/main.zig
+++ src/main.zig
@@ -2,37 +2,44 @@ const std = @import("std");
 
 pub fn main() anyerror!void {}
 
-const STACK_SIZE: usize = 20000;
-var stack = [_]i64{0} ** STACK_SIZE;
-
-var sp: usize = 0;
-fn run(codes: []const []const u8) std.fmt.ParseIntError!void {
-    for (codes) |code| {
-        if (std.mem.eql(u8, code, "+")) {
-            const l = pop();
-            const r = pop();
-            push(l + r);
-        } else {
-            const v = try std.fmt.parseInt(i64, code, 10);
-            push(v);
+fn ForthInterpreter(comptime STACK_SIZE: usize) type {
+    return struct {
+        const Self = @This();
+        stack: [STACK_SIZE]i64 = [_]i64{0} ** STACK_SIZE,
+        sp: usize = 0,
+        pub fn init() Self {
+            return .{};
         }
-    }
-}
+        pub fn run(self: *Self, codes: []const []const u8) !void {
+            for (codes) |code| {
+                if (std.mem.eql(u8, code, "+")) {
+                    const l = self.pop();
+                    const r = self.pop();
+                    self.push(l + r);
+                } else {
+                    const v = try std.fmt.parseInt(i64, code, 10);
+                    self.push(v);
+                }
+            }
+        }
 
-fn pop() i64 {
-    sp -= 1;
-    return stack[sp];
-}
-fn push(v: i64) void {
-    stack[sp] = v;
-    sp += 1;
-}
-fn top() i64 {
-    return stack[0];
+        pub fn pop(self: *Self) i64 {
+            self.sp -= 1;
+            return self.stack[self.sp];
+        }
+        fn push(self: *Self, v: i64) void {
+            self.stack[self.sp] = v;
+            self.sp += 1;
+        }
+        pub fn top(self: *Self) i64 {
+            return self.stack[0];
+        }
+    };
 }
 
 test "run" {
     const code = [_][]const u8{ "1", "2", "+" };
-    try run(code[0..]);
-    try std.testing.expect(top() == 3);
+    var vm = ForthInterpreter(5).init();
+    try vm.run(code[0..]);
+    try std.testing.expect(vm.top() == 3);
 }
