9647bedbbcb8ded58b775a4535ef2685a358118a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-08 07:22:41 +0800

    make them look same first

--- src/compiler.zig
+++ src/compiler.zig
@@ -2,79 +2,24 @@ const std = @import("std");
 const eql = std.mem.eql;
 const Word = @import("./word.zig").Word;
 
-pub fn compile(file: std.fs.File) !void {
-    var asm_f = try std.fs.cwd().createFile(
-        "/tmp/forth.s",
-        .{ .truncate = true },
-    );
-    defer asm_f.close();
-    const w = asm_f.writer();
-
-    var buf: [1024]u8 = undefined;
-
-    const wordsize = 8;
-    var current_offset: isize = 0;
-
-    try w.print(
-        \\.text
-        \\.p2align 2
-        \\
-        \\.global printNumberEntry
-        \\printNumberEntry:
-        \\    mov x15, #10
-        \\    mov x12, x0
-        // print prepare
-        // fd(x0) = 1(stdout)
-        \\    mov x0, #1
-        // len(x2) = 1
-        \\    mov x2, #1
-        // Unix write system call
-        \\    mov x16, #4
-        // x17 for store shift
-        \\    mov x17, #0
-        \\save_loop:
-        // number = x12
-        // x14 = x12 / 10
-        // now x14 is rounded-down quotient of x12
-        \\    udiv x14, x12, x15
-        // x13 = x14 * 10 - x12
-        \\    msub x13, x14, x15, x12
-        // store rounded-down quotient to x12 for next loop
-        \\    mov x12, x14
-        // digit to string
-        \\    add x13, x13, #48
-        \\    strb w13, [sp, x17]
-        \\    sub x17, x17, #4
-        // loop part
-        \\    cmp x12, #0
-        \\    b.eq print_loop
-        \\    b save_loop
-        \\print_loop:
-        \\    add x17, x17, #4
-        \\    add x1, sp, x17
-        \\    svc #0
-        \\    cmp x17, #0
-        \\    b.eq exit
-        \\    b print_loop
-        \\exit:
-        \\    mov x13, #10
-        \\    strb w13, [sp]
-        \\    mov x1, sp
-        \\    svc #0
-        \\    ret
-        \\
-        \\.global _start
-        \\_start:
-        \\entry:
-        \\
-    , .{});
-
-    while (true) {
-        const code = (try file.reader().readUntilDelimiterOrEof(&buf, '\n')) orelse {
-            // No input, probably CTRL-d/EOF.
-            try w.print("ret\n", .{});
-            return;
+pub const Compiler = struct {
+    const Self = @This();
+    asm_file: std.fs.File,
+    pub fn init(a: std.fs.File) Self {
+        return .{
+            .asm_file = a,
         };
+    }
+    pub fn prepare(self: *Self) !void {
+        const w = self.asm_file.writer();
+        try w.print(aarch64_program, .{});
+    }
+    pub fn compile(self: *Self, code: []const u8) !void {
+        const w = self.asm_file.writer();
+
+        const wordsize = 8;
+        var current_offset: isize = 0;
+
         var words = std.mem.tokenize(u8, code, " ");
         var wordIt = words.next();
         while (wordIt != null) : (wordIt = words.next()) {
@@ -147,4 +92,58 @@ pub fn compile(file: std.fs.File) !void {
             }
         }
     }
-}
+};
+
+const aarch64_program =
+    \\.text
+    \\.p2align 2
+    \\
+    \\.global printNumberEntry
+    \\printNumberEntry:
+    \\    mov x15, #10
+    \\    mov x12, x0
+    // print prepare
+    // fd(x0) = 1(stdout)
+    \\    mov x0, #1
+    // len(x2) = 1
+    \\    mov x2, #1
+    // Unix write system call
+    \\    mov x16, #4
+    // x17 for store shift
+    \\    mov x17, #0
+    \\save_loop:
+    // number = x12
+    // x14 = x12 / 10
+    // now x14 is rounded-down quotient of x12
+    \\    udiv x14, x12, x15
+    // x13 = x14 * 10 - x12
+    \\    msub x13, x14, x15, x12
+    // store rounded-down quotient to x12 for next loop
+    \\    mov x12, x14
+    // digit to string
+    \\    add x13, x13, #48
+    \\    strb w13, [sp, x17]
+    \\    sub x17, x17, #4
+    // loop part
+    \\    cmp x12, #0
+    \\    b.eq print_loop
+    \\    b save_loop
+    \\print_loop:
+    \\    add x17, x17, #4
+    \\    add x1, sp, x17
+    \\    svc #0
+    \\    cmp x17, #0
+    \\    b.eq exit
+    \\    b print_loop
+    \\exit:
+    \\    mov x13, #10
+    \\    strb w13, [sp]
+    \\    mov x1, sp
+    \\    svc #0
+    \\    ret
+    \\
+    \\.global _start
+    \\_start:
+    \\entry:
+    \\
+;
--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const eql = std.mem.eql;
 const interpreter = @import("./interpreter.zig");
 const Forth = interpreter.ForthInterpreter;
 const ForthError = interpreter.InterpreterError;
-const compile = @import("./compiler.zig").compile;
+const Compiler = @import("./compiler.zig").Compiler;
 
 pub fn main() anyerror!void {
     var args = std.process.args();
@@ -22,13 +22,37 @@ pub fn main() anyerror!void {
         const must_a_file = args.nextPosix().?;
         var file = try std.fs.cwd().openFile(must_a_file, .{});
         defer file.close();
-        try compile(file);
+
+        try compileStream(file.reader());
     } else {
         // seems like a file!
         var file = try std.fs.cwd().openFile(first.?, .{});
         defer file.close();
 
         try runStream(file.reader());
+    }
+}
+
+fn compileStream(reader: std.io.Reader(std.fs.File, std.os.ReadError, std.fs.File.read)) !void {
+    var asm_f = try std.fs.cwd().createFile(
+        "/tmp/forth.s",
+        .{ .truncate = true },
+    );
+    defer asm_f.close();
+    var compiler = Compiler.init(asm_f);
+    var buf: [1024]u8 = undefined;
+
+    try compiler.prepare();
+
+    while (true) {
+        const code = (try reader.readUntilDelimiterOrEof(&buf, '\n')) orelse {
+            const w = asm_f.writer();
+            try w.print("ret\n", .{});
+            return;
+        };
+        if (compiler.compile(code)) {} else |err| switch (err) {
+            else => return err,
+        }
     }
 }
 
