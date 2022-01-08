281a98b245c0ff07b60c7b4c48c96422fc65a480
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-08 09:29:58 +0800

    handle comment, resolve #6

--- src/interpreter.zig
+++ src/interpreter.zig
@@ -14,52 +14,46 @@ pub fn ForthInterpreter(comptime STACK_SIZE: usize) type {
         pub fn init() Self {
             return .{};
         }
-        pub fn run(self: *Self, code: []const u8) !void {
-            var words = std.mem.tokenize(u8, code, " ");
-            var wordIt = words.next();
-            while (wordIt != null) : (wordIt = words.next()) {
-                const word = try Word.fromString(wordIt.?);
-                switch (word) {
-                    .plus => {
-                        const r = self.pop();
-                        const l = self.pop();
-                        self.push(l + r);
-                    },
-                    .sub => {
-                        const r = self.pop();
-                        const l = self.pop();
-                        self.push(l - r);
-                    },
-                    .mul => {
-                        const r = self.pop();
-                        const l = self.pop();
-                        self.push(l * r);
-                    },
-                    .div => {
-                        const r = self.pop();
-                        const l = self.pop();
-                        self.push(@divTrunc(l, r));
-                    },
-                    .dup => {
-                        const v = self.pop();
-                        self.push(v);
-                        self.push(v);
-                    },
-                    .pop => {
-                        try stdout.print("{} ", .{self.pop()});
-                    },
-                    .print => {
-                        try stdout.print("{} ", .{self.top()});
-                    },
-                    .bye => {
-                        return InterpreterError.Bye;
-                    },
-                    .int => |v| {
-                        self.push(v);
-                    },
-                }
+        pub fn run(self: *Self, word: Word) !void {
+            switch (word) {
+                .plus => {
+                    const r = self.pop();
+                    const l = self.pop();
+                    self.push(l + r);
+                },
+                .sub => {
+                    const r = self.pop();
+                    const l = self.pop();
+                    self.push(l - r);
+                },
+                .mul => {
+                    const r = self.pop();
+                    const l = self.pop();
+                    self.push(l * r);
+                },
+                .div => {
+                    const r = self.pop();
+                    const l = self.pop();
+                    self.push(@divTrunc(l, r));
+                },
+                .dup => {
+                    const v = self.pop();
+                    self.push(v);
+                    self.push(v);
+                },
+                .pop => {
+                    try stdout.print("{} ", .{self.pop()});
+                },
+                .print => {
+                    try stdout.print("{} ", .{self.top()});
+                },
+                .bye => {
+                    return InterpreterError.Bye;
+                },
+                .int => |v| {
+                    self.push(v);
+                },
             }
-            try stdout.print("ok\n", .{});
         }
 
         fn pop(self: *Self) i64 {
@@ -74,16 +68,4 @@ pub fn ForthInterpreter(comptime STACK_SIZE: usize) type {
             return self.stack[0];
         }
     };
-}
-
-test "arith" {
-    var vm = ForthInterpreter(5).init();
-    try vm.run(" 1 2 +");
-    try std.testing.expect(vm.top() == 3);
-    try vm.run("1 - ");
-    try std.testing.expect(vm.top() == 2);
-    try vm.run(" 3 * ");
-    try std.testing.expect(vm.top() == 6);
-    try vm.run("2 /");
-    try std.testing.expect(vm.top() == 3);
 }
--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ const eql = std.mem.eql;
 const interpreter = @import("./interpreter.zig");
 const Forth = interpreter.ForthInterpreter;
 const ForthError = interpreter.InterpreterError;
+const Word = @import("./word.zig").Word;
 
 pub fn main() anyerror!void {
     var args = std.process.args();
@@ -29,15 +30,30 @@ pub fn main() anyerror!void {
 fn runStream(reader: std.io.Reader(std.fs.File, std.os.ReadError, std.fs.File.read)) !void {
     var vm = Forth(2000).init();
     var buf: [1024]u8 = undefined;
+    var in_comment: bool = false;
 
     while (true) {
         const code = (try reader.readUntilDelimiterOrEof(&buf, '\n')) orelse {
             // No input, probably CTRL-d/EOF.
             return;
         };
-        if (vm.run(code)) {} else |err| switch (err) {
-            ForthError.Bye => break,
-            else => return err,
+        var words = std.mem.tokenize(u8, code, " ");
+        var wordIt = words.next();
+        while (wordIt != null) : (wordIt = words.next()) {
+            if (eql(u8, wordIt.?, "(")) {
+                in_comment = true;
+            }
+            if (!in_comment) {
+                const word = try Word.fromString(wordIt.?);
+                if (vm.run(word)) {} else |err| switch (err) {
+                    ForthError.Bye => break,
+                    else => return err,
+                }
+            }
+            if (eql(u8, wordIt.?, ")")) {
+                in_comment = false;
+            }
         }
+        try stdout.print("ok\n", .{});
     }
 }
